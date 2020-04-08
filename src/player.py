7894084642d08bb49b8689a21628477038e7eff1
John Afolabi <johnafolabi.da@gmail.com>
2020-04-08 15:34:10 +0100

    Write class for holding player info

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, curr_room):
+        self.name = name
+        self.curr_room = curr_room
+
+    def __str__(self):
+        output = ""
+        output += self.name + " is in " + self.curr_room
+        return output
