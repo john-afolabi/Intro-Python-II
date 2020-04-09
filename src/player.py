9566be4460b49d0af7855ec6e79eb47728fc1618
John Afolabi <johnafolabi.da@gmail.com>
2020-04-09 15:37:10 +0100

    Define pick and drop item function

--- src/player.py
+++ src/player.py
@@ -3,9 +3,10 @@
 
 
 class Player:
-    def __init__(self, name, curr_room):
+    def __init__(self, name, curr_room, inventory=[]):
         self.name = name
         self.curr_room = curr_room
+        self.inventory = inventory
 
     def __str__(self):
         output = ""
@@ -14,8 +15,21 @@ class Player:
 
     def move(self, direction):
         location = getattr(self.curr_room, str(f"{direction}_to"))
-
         if location:
             self.curr_room = location
         else:
             print("You cannot go there")
+
+    def pick_item(self, item_name):
+        self.item_name = item_name
+        for i, item in enumerate(self.curr_room.items):
+            if item.name.lower() == item_name:
+                self.inventory.append(item)
+                self.curr_room.items.remove(self.curr_room.items[i])
+
+    def drop_item(self, item_name):
+        self.item_name = item_name
+        for i, item in enumerate(self.inventory):
+            if item.name.lower() == item_name:
+                self.curr_room.items.append(item)
+                self.inventory.remove(self.inventory[i])
